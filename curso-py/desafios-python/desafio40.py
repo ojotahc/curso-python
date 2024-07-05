e2f128933fc889e91b203aff04e9c416a3078e37
jotah <ojotahc@gmail.com>
2024-07-05 08:46:40 -0300

    Adicionando desafio 40

---
+++ curso-py/desafios-python/desafio40.py
@@ -0,0 +1,17 @@
+"""
+Crie um programa que leia duas notas de um aluno e calcule sua média, mostrando uma mensagem no final, de acordo com a média atingida:
+- Média abaixo de 5.0: REPROVADO
+- Média entre 5.0 e 6.9: RECUPERAÇÃO
+- Média 7.0 ou superior: APROVADO
+"""
+nota1 = float(input('Digite a primeira nota: '))
+nota2 = float(input('Digite a segunda nota: '))
+media = (nota1 + nota2) / 2
+
+if media < 5.0:
+    print('REPROVADO!')
+elif media >= 5 and media <= 6.9:
+    print('RECUPEREÇÃO!') 
+else:
+    print('APROVADO!')
+print('Tirando {} e {}, a média do aluno é {:.1f}.'.format(nota1, nota2, media))
