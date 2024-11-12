2a6bded9d35b342ddb9437e97e9c13d58748ec80
jotah <ojotahc@gmail.com>
2024-11-12 19:26:13 -0300

    Adicionando aula 13 e 13b

---
+++ curso-py/python-test/aula13.py
@@ -0,0 +1,16 @@
+# se colocar de 1 a 6 ele vai do 1 até o 5 e no 6 ele para, ele não considera o último número
+for c in range(0,6):
+    print('Oi')
+print('FIM')
+
+for c in range(1,7):
+    print(c)
+print('FIM')
+
+for c in range(6, 0, -1):
+    print(c)
+print('FIM')
+
+for c in range(0, 7, 2):
+    print(c)
+print('FIM')
