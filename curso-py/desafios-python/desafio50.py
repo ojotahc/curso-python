1a4ed3728747b147d672cd2346692a0f33861000
jotah <ojotahc@gmail.com>
2024-11-20 15:17:49 -0300

    mudanças no desafio50.py

--- curso-py/desafios-python/desafio50.py
+++ curso-py/desafios-python/desafio50.py
@@ -1,9 +1,11 @@
 '''
 Desenvolva um programa que leia seis números inteiros e mostre a soma apenas daqueles que forem pares. Se o valor digitado for ímpar, desconsidere-o.
 '''
-s = 0 
-for c in range(1,7):
-    num = int(input('Digite um Número: '))
-    if num % 2 == 0:
-        s += num
-print(s)
+soma = 0 # Acumulador é incrementado
+cont = 0 # conta quantas vezes foi incrementado
+for c in range(1, 7):
+  num = int(input('Digite um valor: '))
+  if num % 2 == 0:
+    soma += num
+    cont += 1
+print('Você informou {} números e a soma é {}.'.format(cont, soma))
